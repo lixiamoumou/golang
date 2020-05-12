e4a1fb327a0c9721e8e8353903ded063a7a7fbad
zhangyanhong <291900881@qq.com>
2020-05-12 21:35:54 +0800

    Please enter the commit message for your changes. Lines starting

---
+++ practice/63temporary-files-and-directories.go
@@ -0,0 +1,35 @@
+package main
+
+import(
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+)
+
+func check(err error){
+	if err != nil{
+		panic(err)
+	}
+}
+
+func main()  {
+	f, err := ioutil.TempFile("D:\\code\\Go", "sample")
+	check(err)
+	fmt.Println("temp file name: ", f.Name())
+	//defer os.Remove(f.Name())
+	err = os.Remove(f.Name())
+	fmt.Println(err)
+
+	_,err = f.Write([]byte{1,2,3,4})
+	check(err)
+
+	dname, err := ioutil.TempDir("D:\\code\\Go", "sampledir")
+	check(err)
+	fmt.Println("temp dir name: ", dname)
+	defer os.RemoveAll(dname)
+
+	fname := filepath.Join(dname, "file1")
+	err = ioutil.WriteFile(fname, []byte{1,2}, 0666)
+	check(err)
+}
