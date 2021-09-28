7bc5b9055e8cc634d37d2c2e992bf9ae6dbe227a
lixiamoumou <291900881@qq.com>
2021-09-29 00:52:51 +0800

    httpserver

--- cloudnative/test01/main.go
+++ cloudnative/test01/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"github.com/golang/glog"
 	"io"
 	"log"
 	"net/http"
 	"os/exec"
 )
 
-func indexHandle(w http.ResponseWriter, r *http.Request) {
+func getinfo(w http.ResponseWriter, r *http.Request) {
+	n := fmt.Sprintf("client IP :%s, status code: %s", r.Host, w.Header().Get("status"))
+	fmt.Println(n)
+}
 
+func indexHandle(w http.ResponseWriter, r *http.Request) {
 	rheader := r.Header
+	io.WriteString(w, "========>Details of the http response header<========\n")
 	for k, v := range rheader {
 		w.Header().Set(k, v[0])
-		fmt.Fprintln(w, k, ":", v[0])
+		//fmt.Fprintln(w, k, ":", v[0])
+		io.WriteString(w, fmt.Sprintf("%s:%s\n", k, v))
 	}
+	getinfo(w, r)
 }
 
 func versionHandle(w http.ResponseWriter, r *http.Request) {
@@ -24,10 +33,21 @@ func versionHandle(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 	io.WriteString(w, fmt.Sprintf("%s", ver))
+	getinfo(w, r)
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "ok\n")
+	w.WriteHeader(200)
+	getinfo(w, r)
 }
 
 func main() {
+	flag.Set("v", "4")
+	//flag.Parse()
+	glog.V(0).Info("Starting http server...")
 	http.HandleFunc("/", indexHandle)
 	http.HandleFunc("/version", versionHandle)
+	http.HandleFunc("/healthz", healthz)
 	http.ListenAndServe(":8000", nil)
 }
